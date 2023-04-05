2923de9338d4dc33db044cab201a2cfb8879077b
Артур <119939945+allversace@users.noreply.github.com>
2023-04-06 00:28:49 +0300

    Update Dicriminant.xaml.cs

--- Dicriminant.xaml.cs
+++ Dicriminant.xaml.cs
@@ -20,52 +20,77 @@ namespace WpfApp5
         public MainWindow()
         {
             InitializeComponent();
-            result_x1.IsReadOnly = true;
-            result_x2.IsReadOnly = true;
             a.MaxLength = 3;
             b.MaxLength = 3;
             c.MaxLength = 3;
         }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (a.Text != "" && b.Text != "" && c.Text != "")
-            {
-                int a1 = Convert.ToInt32(a.Text);
-                int b1 = Convert.ToInt32(b.Text);
-                int c1 = Convert.ToInt32(c.Text);
-                int d = (b1 * b1) - (4 * a1 * c1);
+            double searchA = double.Parse(a.Text);
+            double searchB = double.Parse(b.Text);
+            double searchC = double.Parse(c.Text);
 
-                if (d > 0)
-                {
-                    double x1 = (-b1 + Math.Sqrt(d)) / (2 * a1);
-                    double x2 = (-b1 - Math.Sqrt(d)) / (2 * a1);
-                    result_x1.Text = x1.ToString();
-                    result_x2.Text = x2.ToString();
-                }
-                else
-                {
-                    MessageBox.Show("Нет решений D < 0");
-                }
-            }
-            else
+            double D, x, x1, x2;
+
+            D = Math.Pow(searchB, 2) - 4 * searchA * searchC;
+
+            if (D < 0)
+                MessageBox.Show("Корней нет");
+            if (D > 0)
             {
-                MessageBox.Show("Error write");
+                result_x2.Visibility = Visibility.Visible;
+                x1 = (-searchB + Math.Sqrt(D)) / (2 * searchA);
+                x2 = (-searchB - Math.Sqrt(D)) / (2 * searchA);
+                result_x1.Text = x1.ToString();
+                result_x2.Text = x2.ToString();
             }
+            if (D == 0)
+            {
+                x = -searchB / 2 * searchA;
+                result_x1.Text = x.ToString();
+            }
+        }
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void a_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Space)
+                e.Handled = true;
         }
 
         private void a_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            if (!Char.IsDigit(e.Text, 0)) e.Handled = true;
+            if(!Char.IsDigit(e.Text, 0))
+                e.Handled= true;
         }
+
 
         private void b_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            if (!Char.IsDigit(e.Text, 0)) e.Handled = true;
+            if (!Char.IsDigit(e.Text, 0))
+                e.Handled = true;
+        }
+
+        private void b_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+                e.Handled = true;
         }
 
         private void c_PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
-            if (!Char.IsDigit(e.Text, 0)) e.Handled = true;
+            if (!Char.IsDigit(e.Text, 0))
+                e.Handled = true;
+        }
+
+        private void c_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+                e.Handled = true;
         }
     }
 }
